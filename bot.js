0a3644513bcde4ce86a85af2c3442f854e1a7b7d
heexbot <39099888+heexbot@users.noreply.github.com>
2019-02-11 16:36:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,14 +2,32 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 var prefix = "%"
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+
+
+
+
+client.on('message', message => {
+    if (message.author.id === client.user.id) return;
+    if (message.guild) {
+   let embed = new Discord.RichEmbed()
+    let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == prefix + 'bc1') {
+    if (!args[1]) {
+return;
+}
+        message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+            var bc = new Discord.RichEmbed()
+            .addField(' » الرسالة : ', args)
+            .setColor('#ff0000')
+            // m.send(`[${m}]`);
+            m.send(`${m}`,{embed: bc});
+        });
+    }
+    } else {
+        return;
+    }
 });
-
-
-
-
-
 
 
 
